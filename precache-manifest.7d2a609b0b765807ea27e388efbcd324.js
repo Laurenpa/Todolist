84376f7de717bb09cd0889ac2f0d94a4f786ef9b
laurenpa <laurenpan8@gmail.com>
2020-04-13 22:38:03 -0400

    Updates

---
+++ precache-manifest.7d2a609b0b765807ea27e388efbcd324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128b9579cfee10607e394fecad36b1d0",
+    "url": "/Laurenpa/Todolist.git/index.html"
+  },
+  {
+    "revision": "c2fff6e19cfefabc38a6",
+    "url": "/Laurenpa/Todolist.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5c1956815701951f177a",
+    "url": "/Laurenpa/Todolist.git/static/js/2.301046ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Laurenpa/Todolist.git/static/js/2.301046ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2fff6e19cfefabc38a6",
+    "url": "/Laurenpa/Todolist.git/static/js/main.9a77feb6.chunk.js"
+  },
+  {
+    "revision": "95b83733d6b12ad56abc",
+    "url": "/Laurenpa/Todolist.git/static/js/runtime-main.c036c1ac.js"
+  }
+]);
